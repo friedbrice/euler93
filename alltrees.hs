c909c29c2d060d6fa5d4f442b59330d417b77e65
Daniel Brice <danielbrice@gmail.com>
2015-07-06 12:50:58 -0500

    removed alltrees.hs as it wasn't helpful

--- alltrees.hs
+++
@@ -1,8 +0,0 @@
-import Control.Monad
-import Data.List
-
-sizes :: [[FormTree]]
-sizes = [Empty] : (map go . drop 1 . inits) sizes where
-    go smaller = do
-      (ls, rs) <- zip smaller (reverse smaller)
-      liftM2 Node ls rs
