eb4e3740470a066d95ce10f11de90be746a6b6f2
Daniel Brice <danielbrice@gmail.com>
2015-07-06 21:18:43 -0500

    wrote opInsert

--- allAExprs.hs
+++ allAExprs.hs
@@ -1,17 +1,38 @@
 import Control.Applicative (pure, (<*>), (<$>))
 import Data.List (inits, tails)
 
-data Tree x = Null
-            | Leaf x
-            | Node (Tree x) x (Tree x)
-    deriving Show
+-- | a data structure for machine-readable arithmetic expressions
+data AExpr = IntCon !Integer
+           | ABin !ABinOp !AExpr !AExpr
+
+-- | an auxiliary data structure for representing binary ops in AExpr
+data ABinOp = Add
+            | Sub
+            | Mul
+            | Div
+    deriving Eq
+
+-- | replaces an ABinOp token with its common symbol
+instance Show (ABinOp) where
+    show Add = "+"
+    show Sub = "-"
+    show Mul = "*"
+    show Div = "/"
+
+-- | converts an AExpr into human-readable arithmetic
+instance Show (AExpr) where
+    show (IntCon n)    = show n
+    show (ABin op l r) = "(" ++ show l ++ show op ++ show r ++ ")"
 
 splits :: [a] -> [([a], [a])]
+-- ^ utility function returns list of all cuts of the given list
 splits xs = zip (inits xs) (tails xs)
 
-getAllTrees :: [Int] -> [Tree Int]
-getAllTrees []  = [Null]
-getAllTrees [x] = [Leaf x]
-getAllTrees xs  = do
-    (left, x:right) <- splits xs
-    Node <$> getAllTrees left <*> pure x <*> getAllTrees right
+opInsert :: [Integer] -> [AExpr]
+-- ^ returns list of all AExprs that use each member of the given list,
+--   in the order given, exactly once
+opInsert []     = []
+opInsert (x:[]) = [IntCon x]
+opInsert ints   = do
+    (ls, rs) <- splits ints
+    ABin <$> [Add, Sub, Mul, Div] <*> opInsert ls <*> opInsert rs
