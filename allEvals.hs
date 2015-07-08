a5702d558d2104749f513ac8e0fa04c18ac903f9
Daniel Brice <danielbrice@gmail.com>
2015-07-08 02:23:24 -0500

    new and improved

--- allEvals.hs
+++ allEvals.hs
@@ -4,15 +4,11 @@ import Data.List
 splits :: [a] -> [([a], [a])]
 splits xs = init . tail $ zip (inits xs) (tails xs)
 
-applyOp :: (a -> b -> c) -> a -> b -> c
-applyOp op a1 a2 = op a1 a2
-
-opInsert :: [Float] -> [Float]
-opInsert (x:[])   = [x]
-opInsert ints     = do
+opInsert :: [Rational] -> [Rational]
+opInsert [x]  = [x]
+opInsert ints = do
     (ls, rs) <- splits ints
-    applyOp <$> [(+), (-), (*), (/)] <*> opInsert ls <*> opInsert rs
+    [(+), (-), (*), (/)] <*> opInsert ls <*> opInsert rs
 
-allAExprs :: [Float] -> [Float]
-allAExprs = concatMap opInsert . permutations
-
+allEvals :: [Rational] -> [Rational]
+allEvals = concatMap opInsert . permutations
